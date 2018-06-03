2560f722260070fa6d1207a70033e0aea6a06224
pavlo-paliychuk-insighty <pavelko1608@gmail.com>
2018-06-03 11:43:31 +0300

    Add Carter

--- SpriteSheet.js
+++ SpriteSheet.js
@@ -1,8 +1,8 @@
 export default [
-  { name: 'spring_open', x: 34, y: 0, width: 32, height: 39 },
+  { name: 'spring_open', x: 44, y: 0, width: 32, height: 39 },
   { name: 'spring_closed', x: 0, y: 0, width: 33, height: 19 },
   { name: 'grid', x: 0, y: 20, width: 20, height: 20 },
-  { name: 'block_broken', x: 0, y: 41, width: 76, height: 23 },
+  { name: 'block_broken', x: 0, y: 42, width: 76, height: 22 },
   { name: 'block', x: 0, y: 130, width: 76, height: 22 },
-  { name: 'player', x: 0, y: 65, width: 66, height: 64 },
+  { name: 'player', x: 0, y: 66, width: 58, height: 58 },
 ];
